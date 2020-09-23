436eb66361c30268ea50f0d03ca81a86abc4ebc5
singh2210 <saurabh22.techsaga@gmail.com>
2020-09-23 16:31:08 +0530

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -12,13 +12,7 @@ export const products = [
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: 'Phone Standard has been customize'
   }
 ];
 
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
